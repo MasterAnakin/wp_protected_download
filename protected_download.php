6fbc8bcdff1afb344dd68db7d4a811b498179790
MasterAnakin <mbm4816@gmail.com>
2023-08-15 11:42:25 +0200

    Create protected_download.php

---
+++ protected_download.php
@@ -0,0 +1,33 @@
+<?php
+
+//Logic for handling protected downloads on SME site. sme_download_link is a function that will redirect to this file here. There are several security checks to prevent downloading anything else except the media. 
+    
+function download_pdf(){
+    //Check if file exists in media
+    require_once('wp-load.php');
+    global $wpdb;
+    $file = $_GET["link"];
+    $filename = basename($url);
+    $query = "SELECT COUNT(*) FROM {$wpdb->postmeta} WHERE meta_value LIKE '%/$filename'";
+    $count = intval($wpdb->get_var($query));
+
+    if ( $count > 1 AND strpos( $file,'/wp-content/uploads/' ) == true AND strpos( $file,'php' ) == false AND strpos( $file,'txt' ) == false AND strpos( $file,'log' ) == false AND strpos( $file,'pdf' ) == true){
+        //Handle download
+        header( 'Content-Description: File Transfer' );
+        header( 'Content-Type: application/pdf' );
+        header( 'Content-Disposition: attachment; filename=' . basename($file) );
+        header( 'Content-Transfer-Encoding: binary' );
+        header( 'Expires: 0' );
+        header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+        header( 'Pragma: public' );
+        header( 'Content-Length: ' . filesize($file) );
+        ob_clean();
+        flush();
+        readfile( $file );
+        exit;
+    }
+
+}
+download_pdf();
+
+
